3a04bc1b945ad07f76cb8b82ba45ddbc6859e786
Emerson Auguste <emerson56@gmail.com>
2016-09-29 00:15:16 -0400

    Rename file to TipViewController from ViewController

---
+++ TipCalculator/TipViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Emerson on 9/23/16.
+//  Copyright © 2016 Emerson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //Load from settings
+        let defaults = UserDefaults.standard
+        tipControl.selectedSegmentIndex = defaults.integer(forKey: "tip_default_percentage")
+        billField.becomeFirstResponder()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onTap(_ sender: AnyObject) {
+        //view.endEditing(true)
+        self.billField.alpha = 0
+        let animations = {
+            self.billField.alpha = 1
+        }
+        
+        UIView.animate(withDuration: 1, animations: animations)
+
+    }
+    
+    @IBAction func calculateTip(_ sender: AnyObject) {
+        
+        let tipPercentages = [0.18, 0.20, 0.25]
+        
+        let bill = Double (billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$ %.2f", tip)
+        totalLabel.text = String (format: "$ %.2f", total)
+        
+    }
+}
+
